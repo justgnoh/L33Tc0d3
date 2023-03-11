dfbefc51a707849391c2f633612b021f5fa1fbf6
justgnoh <justingnoh@gmail.com>
2023-03-11 17:13:04 +0800

    Add notes for Min AND ^ OR question

--- HackerEarth/MinimumAND-XOR-OR.js
+++ HackerEarth/MinimumAND-XOR-OR.js
@@ -1,3 +1,14 @@
+/**
+ * Notes:
+ * - (A & B) ^ (A || B) is the same as (A) ^ (B)
+ * - (A && B) ^ (A || B)= A ^(A||B) && B^(A||B) = A^B && B^A = A^B
+ * 
+ * Sort function here converts everything to UTF-16 string and compares the first character of the string. Hence the callback
+ * must be used explicitly catered for numbers.
+ * 
+ * More info here: https://matcha.fyi/sort-not-working-javascript/#:~:text=The%20array%20returned%20is%20hardly,there%20is%20no%20callback%20specified.
+ */
+
 process.stdin.resume();
 process.stdin.setEncoding("utf-8");
 var stdin_input = "";
